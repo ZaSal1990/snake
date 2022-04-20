948d9b5bc98c1230fc58f35eb07048aa7ed6d4c4
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-20 14:46:25 -0600

    import conecting fns from a sperate file and exporting module to play.js

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 
 // establishes a connection with the game server
-const net = require('net');
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('connect', (data) => {
-    console.log(data);
-  });
-  return conn;
-};
-
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+
+
 
